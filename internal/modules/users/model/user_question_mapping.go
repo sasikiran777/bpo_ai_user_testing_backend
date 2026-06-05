f4976f15f73aad073145194d1941ca64697f8331
Sasi <sasiking93@gmail.com>
2026-06-05 19:12:16 +0530

    ss

--- internal/modules/users/model/user_question_mapping.go
+++ internal/modules/users/model/user_question_mapping.go
@@ -15,7 +15,7 @@ type UserQuestionMapping struct {
 	UserAnswer           []string  `bun:"user_answer,type:jsonb,notnull,default:'[]'::jsonb"`
 	TestNotes            []string  `bun:"test_notes,type:jsonb,notnull,default:'[]'::jsonb"`
 	MarksObtained        int       `bun:"marks_obtained"`
-	AIFeedback           string    `bun:"aif_feedback,type:text"`
+	AIFeedback           string    `bun:"ai_feedback,type:text"`
 	ChangedWindowsCount  int       `bun:"changed_windows_count"`
 	HasGraded            bool      `bun:"has_graded,notnull,default:false"`
 }
